64a1a1057d8a653be57ef918962a7eaf105f4b0c
Brady Koehler <me@bradykoehler.com>
2019-03-09 22:07:58 -0700

    Remove console output

--- public/address.js
+++ public/address.js
@@ -7,7 +7,6 @@ $(function() {
         alert(res.error);
         return;
       }
-      console.log(res);
 
       if (res.valid) {
         $('#verified_address').html("ADDRESS VERIFIED:<br /><br />" + res.address.filter(Boolean).join('<br />'));
